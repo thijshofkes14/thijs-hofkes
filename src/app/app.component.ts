e5f12ff3c844dbe79767b9c023fa3278149fb93f
Thijs Hofkes <thijssiehofkes@gmail.com>
2025-05-12 14:49:21 +0200

    Basic home update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'thijs-hofkes';
 }
