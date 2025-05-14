47b76a80e7c756cd40fb0d4bb18c2464798b867a
Thijs Hofkes <thijssiehofkes@gmail.com>
2025-05-14 10:43:16 +0200

    Added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material/material.module';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
